ae8216ec8e26f7ededd94b22947027da24b71bb3
Maggie Gutierrez <maggie.vibes@icloud.com>
2026-09-25 10:23:51 -0600

    Description for the projects completed and some styles on this endpoint finished

--- server/src/seed/data/projects.data.js
+++ server/src/seed/data/projects.data.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: "Around the México",
     description:
-      "Save the best experiences's pics and share them with your friends. Built with React, Node.js, Express, and MongoDB.",
+      "Save the best experiences's pics and share them with your friends. Built with React, Node.js, Express, and MongoDB, using JWT secret for better user security. Deployed on Google Cloud",
     githubUrl: "https://github.com/maggiegutierrez/web_project_api_full",
     liveUrl: "https://maggie.h4ck.me/signin",
     thumbnailUrl: "/assets/images/projects/work_1.png",
@@ -10,7 +10,7 @@ export const projects = [
   {
     title: "Find Me A Job",
     description:
-      "Register a user with an invented email, look for some job options and save the ones are interesting for you. Built with React, Node.js, Express, and MongoDB",
+      "Register a user with any email, look for some job options and save the ones are interesting for you. Built with React, Node.js, Express, and MongoDB, using JWT secret for better user security. It was used the external API 'The Muse'. Deployed on Firebase.",
     githubUrl: "https://github.com/maggiegutierrez/FIND_ME_A_JOB",
     liveUrl: "https://project-ef82d4ab-d03e-4bdb-b4a.web.app/",
     thumbnailUrl: "/assets/images/projects/findMeAJob.png",
@@ -18,7 +18,7 @@ export const projects = [
   {
     title: "De patria a patria",
     description:
-      "A place to know some of the TripleTen employees's residencies. Built with HTML and CSS.",
+      "A place to know some of the TripleTen employees's residencies. Built with HTML and CSS. The goal here wa to master the Grid and Flexbox concepts, and to learn how to use the CSS variables. Deployed on GitHub Pages.",
     githubUrl: "https://github.com/maggiegutierrez/web_project_homeland",
     liveUrl: "https://maggiegutierrez.github.io/web_project_homeland/",
     thumbnailUrl: "/assets/images/projects/patriaApatria.png",
@@ -26,7 +26,7 @@ export const projects = [
   {
     title: "Portafolio FullStack",
     description:
-      "A personal portfolio to show my skills and projects, built with React, Node.js, Express, and MongoDB.",
+      "A personal portfolio to show the software I can use and my projects, as well as the art (Graphic design and photography) I can make. Built with React, Node.js, Express, and MongoDB. Deployed on Vercel.",
     githubUrl: "https://github.com/maggiegutierrez/max_portfolio",
     liveUrl: "https://maggiegutierrez.github.io/web_project_homeland/",
     thumbnailUrl: "/assets/images/projects/portfolioFullstack.png",
